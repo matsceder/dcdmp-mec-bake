053f3ee0dbe49f3c9f230e40ae4af5d9268143e5
matsceder <mats_ceder@hotmail.com>
2020-05-18 08:24:19 +0000

    now connected with mongodb and tested reading from db. also added basic template files for the different pages

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template  # , redirect, request, url_for
+from flask_pymongo import PyMongo
+# from bson import ObjectId
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'bake'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("recipe-db.html", recipe=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
