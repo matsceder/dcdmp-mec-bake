eadff657d67b8c5a152cec1878dea480656d25af
matsceder <mats_ceder@hotmail.com>
2020-05-18 14:24:10 +0000

    recipe-new now writes to mongodb. though not correctly to nested lists yet. started on recipe-update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template  # , redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-# from bson import ObjectId
+from bson import ObjectId
 
 
 app = Flask(__name__)
@@ -14,15 +14,37 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
+@app.route('/get_recipes')
+def get_recipes():
     return render_template("recipe-db.html",
                             recipe=mongo.db.recipe.find())
 
 
-@app.route('/new_recipe')
-def new_recipe():
+@app.route('/recipe_view')
+def recipe_view():
+    return render_template("recipe-view.html",
+                            recipe=mongo.db.recipe.find())
+
+
+@app.route('/recipe_new')
+def recipe_new():
     return render_template("recipe-new.html",
+                            recipe_type=mongo.db.recipe_type.find(),
+                            recipe_diff=mongo.db.recipe_diff.find(),
+                            recipe=mongo.db.recipe.find(),
+                            ingredient_units=mongo.db.ingredient_units.find())
+
+
+@app.route('/send_new', methods=['POST'])
+def send_new():
+    recipe = mongo.db.recipe
+    recipe.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/recipe_update')
+def recipe_update():
+    return render_template("recipe-update.html",
                             recipe_type=mongo.db.recipe_type.find(),
                             recipe_diff=mongo.db.recipe_diff.find(),
                             recipe=mongo.db.recipe.find(),
