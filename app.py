3952838af6cdc8b7aac502c22dbe3c04e9d2de9d
matsceder <mats_ceder@hotmail.com>
2020-05-27 16:11:55 +0000

    now it's possible to nicely list nested object strings from the db

--- app.py
+++ app.py
@@ -45,10 +45,19 @@ def recipe_db_t_d(type_r, diff_r):
                             recipe_diff=recipe_diffs)
 
 
-@app.route('/recipe_view')
-def recipe_view():
-    return render_template("recipe-view.html",
-                            recipe=mongo.db.recipe.find())
+@app.route('/recipe_view/<recipe_id>')
+def recipe_view(recipe_id):
+    the_recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
+    rec_ing = the_recipe["rec_ing"]
+    ing_names = the_recipe["rec_ing"]["ing_name"]
+    all_types = mongo.db.recipe_type.find()
+    all_diffs = mongo.db.recipe_diff.find()
+    return render_template('recipe-view.html',
+                            recipe=the_recipe,
+                            recipe_type=all_types,
+                            recipe_diff=all_diffs,
+                            all_ing=rec_ing,
+                            ing_name=ing_names)
 
 
 @app.route('/recipe_new')
@@ -63,16 +72,48 @@ def recipe_new():
 @app.route('/send_new', methods=['POST'])
 def send_new():
     recipe = mongo.db.recipe
+    steps = mongo.db.steps
+    ingredients = mongo.db.ingredients
     recipe.insert_one(
         { "rec_name" : request.form['rec_name'],
         "rec_aut" : request.form['rec_aut'],
         "rec_type" : request.form['rec_type'],
         "rec_diff" : request.form['rec_diff'],
-        "rec_time" : { "time_h" : request.form['time_h'], "time_m" : request.form['time_m'] },
-        "rec_equip" : { "equip_name" : request.form['equip_name'], "equip_aff" : request.form['equip_aff'], "equip_desc" : request.form['equip_desc'] },
-        "rec_ing" : { "ing_name" : request.form['ing_name'], "ing_amount" : request.form['ing_amount'], "ing_unit" : request.form['ing_unit'] },
-        "rec_step" : { "step_num" : request.form['step_num'], "step_desc" : request.form['step_desc'] },
+        "rec_time_h" : request.form['time_h'],
+        "rec_time_m" : request.form['ing_amount'],
         "rec_pic" : request.form['rec_pic']
+        }
+    )
+    steps.insert_one(
+        { "rec_name" : request.form['rec_name'],
+        "rec_step_1" : request.form['rec_step_1'],
+        "rec_step_2" : request.form['rec_step_2'],
+        "rec_step_3" : request.form['rec_step_3'],
+        "rec_step_4" : request.form['rec_step_4'],
+        "rec_step_5" : request.form['rec_step_5'],
+        "rec_step_6" : request.form['rec_step_6'],
+        "rec_step_7" : request.form['rec_step_7'],
+        "rec_step_8" : request.form['rec_step_8'],
+        "rec_step_9" : request.form['rec_step_9'],
+        "rec_step_10" : request.form['rec_step_10'],
+        "rec_step_11" : request.form['rec_step_11'],
+        "rec_step_12" : request.form['rec_step_12']
+        }
+    )
+    ingredients.insert_one(
+        { "rec_name" : request.form['rec_name'],
+        "rec_ing_1" : request.form['rec_ing_1'],
+        "rec_ing_2" : request.form['rec_ing_2'],
+        "rec_ing_3" : request.form['rec_ing_3'],
+        "rec_ing_4" : request.form['rec_ing_4'],
+        "rec_ing_5" : request.form['rec_ing_5'],
+        "rec_ing_6" : request.form['rec_ing_6'],
+        "rec_ing_7" : request.form['rec_ing_7'],
+        "rec_ing_8" : request.form['rec_ing_8'],
+        "rec_ing_9" : request.form['rec_ing_9'],
+        "rec_ing_10" : request.form['rec_ing_10'],
+        "rec_ing_11" : request.form['rec_ing_11'],
+        "rec_ing_12" : request.form['rec_ing_12']
         }
     )
     return redirect(url_for('recipe_db'))
